eb5888c85bd2ed8895135e884d39246b9000e8ea
otobongfp <otobongfp@gmail.com>
2023-03-18 15:24:44 +0100

    made code more efficient

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,38 @@
 use std::io;
-use rand::Rng;
 use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
-    println!("________A GUESSING GAME_________");
-    println!("Guess a number between 1 and 10:");
+    println!("A GUESSING GAME BUILT WITH RUST");
 
-    let _secret_number = rand::thread_rng().gen_range(1..=10);
+    println!("Pick a number between 1 and 10");
 
 
-    loop{
+    //Random Number Generator
+    let _secret_number :u32 = rand::thread_rng().gen_range(1..=10);
 
+
+    loop {
+        //Variable to collect the guess
         let mut guess = String::new();
 
-        io::stdin()
-        .read_line(&mut guess)
-        .expect("Did you pass in a number?");
+        //read the input from the cli
+        io::stdin().read_line(&mut guess).expect("something went wrong");
 
+        //convert guess from string to Int
         let guess : u32 = match guess.trim().parse(){
-            Ok(num) => num, 
+            Ok(num) => num,
             Err(_) => continue,
         };
 
-        match guess.cmp(&_secret_number){
-            Ordering::Greater => println!("{guess} is bigger than the secret number, Try Again 😥"),
-            Ordering::Less => println!("{guess} is smaller than the secret number, Try Again 😥"),
+        match guess.cmp(&_secret_number) {
+            Ordering::Less => println!("{} is smaller than secret number 😥", guess),
+            Ordering::Greater => println!("{} is bigger than the secret number 😥", guess),
             Ordering::Equal => {
-                println!("Correct, You Got It!!! 😀");
+                println!("Correct, you got it 😀");
                 break;
             },
         }
     }
+
 }
